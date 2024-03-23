928b09ece6a5a22b52b11180d82d570f405cdfb3
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-03-23 14:09:58 +0700

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import logo from './logo.svg';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello There</h1>
-      <ul>
-        <li>1. General Kenobi!</li>
-        <li>2. You are a bold one!</li>
-        <li>3. Kill him!</li>
-      </ul>
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <nav className="font-mono py-4 shadow-lg">
+                    <div>
+                        <h1 className="text-4xl font-bold text-center underline underline-offset-8"><span className="text-orange-500 underline underline-offset-8">Reddit</span>Minimal</h1>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
