a8f0c62f8e85691e5826bfbc220fcca7a029d23c
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-03-23 22:15:23 +0700

    create post container

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'post': '80px auto',
+      }
+    },
   },
   plugins: [],
 }
